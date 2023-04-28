17fd35f09dcf7f60f055be76b9c576d51776f68e
MAYSA SILVA <maysa.silva4@fatec.sp.gov.br>
2023-04-28 20:21:12 -0300

    alterando a parte de cadastro

--- WebApp/Controllers/ClienteController.cs
+++ WebApp/Controllers/ClienteController.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class ClienteController : Controller
     {
+        public List<ClientesModel> db = new List<ClientesModel>();  
         public IActionResult Lista()
         {
-            return View();
+            return View(db);
         }
 
         public IActionResult Cadastro()
         {
-            return View();
+            ClientesModel model=new ClientesModel();
+            return View(model);
+        }
+
+        //rota que salva os dados
+        [HttpPost]
+        public IActionResult SalvarDados(ClientesModel cliente)
+        {
+            if(cliente.Id==0)
+            {
+                Random rand = new Random();
+                cliente.Id = rand.Next(1, 999);
+                db.Add(cliente);
+            }
+            return RedirectToAction("Lista");
         }
     }
 }
---
+++ WebApp/Models/ClientesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class ClientesModel //classe responsável por pegar informações do back para o front
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+
+
+    }
+}
